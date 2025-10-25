43bf42643e54825789a6262c3bc3ac6f4d03358e
arturojimenezg15 <arturojimenezg15@gmail.com>
2025-10-25 11:11:04 -0400

    model blog added

---
+++ blogs/admin.py
@@ -0,0 +1,10 @@
+from django.contrib import admin
+from .models import Publication 
+# Register your models here.
+class PublicationAdmin(admin.ModelAdmin):
+    list_display = ('title', 'author', 'published_date', 'updated_date')
+    search_fields = ('title', 'content', 'author__username')
+list_filter = ('published_date', 'update_date' 'author')
+
+
+admin.site.register(Publication, PublicationAdmin)
--- django_base/settings.py
+++ django_base/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-]
+    'blogs',
+    ] 
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
